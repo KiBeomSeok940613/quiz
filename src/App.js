8187defe32bef933f43a4109642068a1472380db
기범석 <kbs9406@gmail.com>
2023-08-30 15:53:40 +0900

    퀴즈의신

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import "./Test.scss"
 import { Route, Routes } from "react-router-dom";
 import Main from './pages/Main';
 import Detail from './pages/Detail';
+import { useState } from 'react';
+import { data } from 'autoprefixer';
+import QuizList from './components/Quizlist';
 
+ 
 
 
 function App() {
-  return (
+  console.log(QuizList)
+    const QuizLength = QuizList.length;
+    console.log(QuizLength)
+    const [userName, setUserName] = useState("");
+    const[quizList, setQuizList] = useState(QuizList);
+    const ChangeName = (data) =>{
+      setUserName(data);
+    }    
+    
+    return (
     <>
       <Routes>
-        <Route path='/' element={<Main/>}/>
-        <Route path='/' element={<Detail/>}/>
+        <Route path='/' element={<Main ChangeName={ChangeName} userName={userName} QuizLength={QuizLength} quizList={quizList}/>}/>
+        <Route path='/quiz' element={<Detail quizList={quizList} userName={userName}/>}/>
         
       </Routes>
     </>
---
+++ src/components/Quizlist.js
@@ -0,0 +1,199 @@
+const QuizList = [
+    {
+        id: 1,
+        question: "HTML의 의미를 보기에서 고르세요!",
+        answer: "Hyper Text Markup Language",
+        view: {
+            number1 : "Hyper Text Markup Language",
+            number2 : "Hyperlinks and text Markup Language",
+            number3 : "home tool Markup Language",
+            number4 : "Hyperlinks Tool Markup Language",
+        },
+        hint : "하이퍼텍스트 마크업 언어입니다.",
+        type: "html"
+    },
+    {
+        id: 2,
+        question: "다음 속성에 대한 설명 중 틀린 것을 고르세요.",
+        answer: "alt 속성은 요소에 대한 설명으로 마우스를 호버하면 툴팁을 표시합니다.",
+        view: {
+            number1 : "src 속성은 필요한 소스의 경로를 나타냅니다",
+            number2 : "href 속성은 하이퍼링크가 가리키는 URL을 나타냅니다.",
+            number3 : "alt 속성은 요소에 대한 설명으로 마우스를 호버하면 툴팁을 표시합니다.",
+            number4 : "lang 속성은 요소 내에 사용된 언어를 정의합니다.",
+        },
+        hint : "없음",
+        type: "html"
+    },
+    {
+        id: 3,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 4,
+        question: "다음은 flex에 대한 설명입니다. 올바르지 않은 설명을 하는 사람을 고르세요.",
+        answer: "flex-item들은 기본적으로 Axis 기준으로 정렬되기 때문에 기본 방향은 위쪽에서 아래쪽이야!",
+        view: {
+            number1 : " 은주 : flex는 부모를 통해 자식들을 컨트롤할 수 있어!",
+            number2 : " 창욱 : 그리고 직계자식까지만 영향을 줄 수 있지.",
+            number3 : "윤정 : flex-item들은 기본적으로 Axis 기준으로 정렬되기 때문에 기본 방향은 위쪽에서 아래쪽이야!",
+            number4 : "범석 : flex-direction이 column일 때 가로로 중앙정렬을 하고 싶다면 align-item을 center로 설정하면 된대.",
+        },
+        hint : "flex는 신이야",
+        type: "css"
+    },
+    {
+        id: 5,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 6,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 7,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 8,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 9,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 10,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 11,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 12,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 13,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 14,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+    {
+        id: 15,
+        question: "HTML 요소에 대한 설명으로 틀린 것을 고르세요",
+        answer: "태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+        view: {
+            number1 : "<p> 태그는 문단을 나타냅니다.",
+            number2 : "<head> 태그는 소개 및 탐색에 도움을 주는 콘텐츠를 나타냅니다.",
+            number3 : "<article> 태그는 사이트 안에서 독립적으로 구분해 재사용할 수 있는 구획을 의미합니다.",
+            number4 : "<body> 태그는 html 문서의 내용을 나타냅니다.",
+        },
+        hint : "알아서 맞추세요.",
+        type: "html"
+    },
+]
+
+export default QuizList;
